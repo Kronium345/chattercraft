7e0870cc27a38d7981cc75da0238079bbdd6d26f
Daniel Awolowo <p2575402@my365.dmu.ac.uk>
2023-11-30 10:46:56 +0000

    first commit

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,28 @@
+import * as z from "zod";
+
+export const SignUpValidation = z.object({
+    name: z.string().min(2, { message: "Too short. Try again "}),
+    username: z.string().min(2, { message: "Too short. Try again"}),
+    email: z.string().email(),
+    password: z.string().min(8, { message: "Too short. Password must be at least 8 characters. Try again"}),
+  });
+
+export const SignInValidation = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, { message: "Too short. Password must be at least 8 characters. Try again"}),
+  });
+
+export const PostValidation = z.object({
+    caption: z.string().min(5).max(2200),
+    file: z.custom<File[]>(),
+    location: z.string().min(2).max(100),
+    tags: z.string(),
+  });
+
+  export const ProfileValidation = z.object({
+    file: z.custom<File[]>(),
+    name: z.string().min(2, { message: "Name must be at least 2 characters." }),
+    username: z.string().min(2, { message: "Name must be at least 2 characters." }),
+    email: z.string().email(),
+    bio: z.string(),
+  });
